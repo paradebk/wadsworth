ae18e1eaab7c48da7138f9ae17a892ed06732057
kdietz <kdietz65@gmail.com>
2026-05-14 22:48:33 -0500

    Add icons for popular file extensions

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,7 +2,11 @@ import './assets/main.css'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { addCollection } from '@iconify/react'
+import vscodeIcons from '@iconify-json/vscode-icons/icons.json'
 import App from './App'
+
+addCollection(vscodeIcons)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
